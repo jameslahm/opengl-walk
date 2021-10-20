227cf7dbb37fe070e5131d5b8cfe40bcab5023a4
jameslahm <1366463855@qq.com>
2021-10-20 11:19:38 +0800

    feat: add model save and read

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -17,6 +17,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
+int load();
 unsigned int loadTexture(const char* path);
 
 // settings
@@ -36,6 +37,11 @@ float lastFrame = 0.0f;
 
 int main()
 {
+
+    {
+        // load();
+        // return 0;
+    }
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -118,7 +124,7 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
     float planeVertices[] = {
-        // positions          // texture Coords 
+        // positions          // texture Coords
          5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
         -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
         -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
@@ -218,6 +224,26 @@ int main()
 	// load models
 	Model earthModel("tree/tree.obj");
     Model earthModel2("earth/earth.obj");
+
+    ofstream outfile;
+    outfile.open("model.txt");
+    outfile << "V 2 36" << endl;
+    outfile << "0 1" << endl;
+    for (int i = 0; i < 180 / 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            outfile << cubeVertices[i * 5 + j] << " ";
+        }
+        outfile << endl;
+    }
+    outfile << "M " << "earth/earth.obj" << endl;
+    outfile << "0 0 0" << endl;
+    outfile << "20 20 20" << endl;
+    outfile << "M " << "tree/tree.obj" << endl;
+    outfile << "-3.0 -0.5 -1.0" << endl;
+    outfile << "1.0 1.0 1.0" << endl;
+    outfile.flush();
+    outfile.close();
+
 
     bool is_reverse = false;
 
@@ -481,4 +507,328 @@ unsigned int loadTexture(char const* path)
     return textureID;
 }
 
+int load()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+
+    ifstream infile;
+    infile.open("model.txt");
+    vector<Shader*> shaders;
+    vector<Model> models;
+    vector<Shader*> modelShaders;
+    vector<int> VAOS;
+    vector<vector<float>> translates;
+    vector<vector<float>> scales;
+
+
+    while (infile.peek() != EOF) {
+        string type;
+        infile >> type;
+        if (type == "V" || type == "P") {
+            int textures_num;
+            int vertices_num;
+            infile >> textures_num;
+
+            infile >> vertices_num;
+            vector<int> textures;
+            for (int i = 0; i < textures_num; i++) {
+                int in;
+                infile >> in;
+                textures.push_back(in);
+            }
+            float* vertexs = (float*)malloc(sizeof(float) * vertices_num * 5);
+            for (int i = 0; i < vertices_num * 5; i++) {
+                float f1;
+                infile >> f1;
+                vertexs[i] = f1;
+
+            }
+            unsigned int VAO, VBO;
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glBindVertexArray(VAO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices_num * 5, vertexs, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+            Shader* shader = new Shader("shaders/vertex.txt", "shaders/fragment.txt");
+            shader->use();
+            for (int i = 0; i < textures_num; i++) {
+                shader->setInt("texture" + to_string(i + 1), textures[i]);
+            }
+            shaders.push_back(shader);
+            VAOS.push_back(VAO);
+        }
+        else if (type == "M") {
+            Shader* modelShader = new Shader("shaders/vertex_model.txt", "shaders/fragment_model.txt");
+            modelShader->use();
+            modelShader->setInt("texture1", 0);
+            modelShaders.push_back(modelShader);
+            string name;
+            infile >> name;
+            Model model(name.c_str());
+            models.push_back(model);
+            vector<float> t(3);
+            infile >> t[0];
+            infile >> t[1];
+            infile >> t[2];
+            translates.push_back(t);
+            vector<float> s(3);
+            infile >> s[0];
+            infile >> s[1];
+            infile >> s[2];
+            scales.push_back(s);
+        }
+    }
+
+    float planeVertices[] = {
+        // positions          // texture Coords 
+         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+         5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+    };
+    // plane VAO
+    unsigned int planeVAO, planeVBO;
+    glGenVertexArrays(1, &planeVAO);
+    glGenBuffers(1, &planeVBO);
+    glBindVertexArray(planeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+
+
+    Shader shaderCollision("shaders/vertex_collision.txt", "shaders/fragment_collision.txt");
+    Shader screenShader("shaders/vertex_screen.txt", "shaders/fragment_screen.txt");
+
+    float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
+    };
+    // screen quad VAO
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+
+    shaderCollision.use();
+    shaderCollision.setInt("texture1", 0);
+    shaderCollision.setInt("texture2", 1);
+
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
+
+    // framebuffer configuration
+    // -------------------------
+    unsigned int framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    // create a color attachment texture
+    unsigned int textureColorbuffer;
+    glGenTextures(1, &textureColorbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+    // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT); // use a single renderbuffer object for both a depth AND stencil buffer.
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    unsigned int cubeTexture = loadTexture("container.jpg");
+    unsigned int floorTexture = loadTexture("floor.jpg");
+    unsigned int faceTexture = loadTexture("awesomeface.png");
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        glEnable(GL_DEPTH_TEST);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+        for (int i = 0; i < shaders.size(); i++) {
+            Shader& shader = *shaders[i];
+            shader.use();
+            glm::mat4 model = glm::mat4(1.0f);
+            glm::mat4 view = camera.GetViewMatrix();
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            shader.setMat4("view", view);
+            shader.setMat4("projection", projection);
+            // cubes
+            glBindVertexArray(VAOS[0]);
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, faceTexture);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, cubeTexture);
+            // glActiveTexture(GL_TEXTURE1);
+            // glBindTexture(GL_TEXTURE_2D, faceTexture);
+            model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+            shader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+            shader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(-1.5f, 0.0f, 0.5f));
+            model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+            float delta = sin((float)glfwGetTime()) / 4;
+            model = glm::translate(model, glm::vec3(delta, delta + 0.25, delta));
+            shader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
+
+            glBindVertexArray(planeVAO);
+            glBindTexture(GL_TEXTURE_2D, floorTexture);
+            shader.setMat4("model", glm::scale(glm::mat4(1.0f), glm::vec3(10.0f, 1.0f, 10.0f)));
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+            glBindVertexArray(0);
+
+            // Collision
+            shaderCollision.use();
+            shaderCollision.setMat4("view", view);
+            shaderCollision.setMat4("projection", projection);
+            glBindVertexArray(VAOS[0]);
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, faceTexture);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, cubeTexture);
+
+            delta = sin((float)glfwGetTime()) / 4;
+            bool is_collision = false;
+            bool collisionX = delta + 1 >= 1 - delta && 1 - delta + 1 >= delta;
+            bool collisionY = delta + 0.25 + 1 >= delta + 0.25 &&
+                delta + 0.25 + 1 >= delta + 0.25;
+            bool collisionZ = delta + 1 >= delta && delta + 1 >= delta;
+            is_collision = collisionX && collisionY && collisionZ;
+            shaderCollision.setBool("collision", is_collision);
+
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(0.5f, 0.0f, 1.5f));
+
+            model = glm::translate(model, glm::vec3(delta, delta + 0.25, delta));
+            shaderCollision.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(1.5f, 0.0f, 1.5f));
+            model = glm::translate(model, glm::vec3(-delta, delta + 0.25, delta));
+            shaderCollision.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+
+        for (int i = 0; i < modelShaders.size(); i++) {
+            Model objModel = models[i];
+            Shader& shader = *modelShaders[i];
+            glm::mat4 model = glm::mat4(1.0f);
+            shader.use();
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, cubeTexture);
+            shader.setMat4("view", view);
+            shader.setMat4("projection", projection);
+            // render the loaded model
+
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(translates[i][0], translates[i][1], translates[i][2]));
+            model = glm::scale(model, glm::vec3(scales[i][0], scales[i][1], scales[i][2]));
+            shader.setMat4("model", model);
+            objModel.Draw(shader);
+        }
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glDisable(GL_DEPTH_TEST);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        screenShader.use();
+        glBindVertexArray(quadVAO);
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    for (int i = 0; i < VAOS.size(); i++) {
+        glDeleteVertexArrays(1, (GLuint*)&VAOS[i]);
+    }
+
+    glDeleteVertexArrays(1, &quadVAO);
+    glDeleteBuffers(1, &quadVBO);
+
+    glfwTerminate();
+    return 0;
+}
 
